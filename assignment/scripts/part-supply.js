bdd1fd75526813e7c7a7ca62be1f8e26e7db174f
Francisco Lopez <frankie.b.lopez@gmail.com>
2021-08-15 17:51:12 -0500

    completed Prime Tier 1 Week 3 - Part 3

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -1,25 +1,50 @@
 console.log('****** Part Supply *******');
 // REQUIRED FEATURES
 
-// 1. Create a variable called 'partsNeeded' with a value of the number 40 
+// 1. Create a variable called 'partsNeeded' with a value of the number 40
+ let partsNeeded = 40; 
 //    & console.log the variable
-console.log('1. Number of partsNeeded:');
+            // '1. Number of partsNeeded:'
+console.log( partsNeeded );
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
+let supplyChanges = [ 3, 5, -6, 0, 7, 11 ];
 //    the following numbers: 3, 5, -6, 0, 7, 11
-console.log('2. Array of supplyChanges:');
+            // 2. Array of supplyChanges:
+console.log( 'supplyChanges', supplyChanges );
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
-console.log('3. Second supplyChange is:');
+            // 3. Second supplyChange is:
+console.log( supplyChanges[1] );
 
-// 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
+// 4. The last item was added by mistake. Remove it from the 'supplyChanges'
+let removedItem = supplyChanges.pop( '11' );{
+    console.log( 'removed item:', removedItem );
+}
 //    array & console.log the value removed.
-console.log('4. Removed item:');
+
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
-console.log('5. Adding 25 to supplyChanges.');
+supplyChanges.push( 25 );{
+console.log( 'supplyChanges', supplyChanges );
+}
+            // 5. Adding 25 to supplyChanges.
+// console.log( '25' );
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
+let x = supplyChanges;
+for( let i=0; i<x.length; i++){
+    console.log( 'supplyChanges', x[i] );
+    if( x[i] === 0 ){
+        console.log( 'no change') ;
+    }
+    if( x[i] > 0 ){
+        console.log( 'added', x[i], 'parts' );
+    }
+    if( x[i] < 0  ){
+        console.log( 'Removed', x[i], 'parts')
+    }
+}
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.' 
 //    - if the value is 0, log 'No Change.'
